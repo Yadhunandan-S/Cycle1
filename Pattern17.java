e5b65acc0ea179cfb9f08efaa0a4d121e1250997
Yadhunandan S <139615870+Yadhunandan-S@users.noreply.github.com>
2024-02-18 22:49:40 +0530

    Create Pattern17.java

---
+++ Pattern17.java
@@ -0,0 +1,23 @@
+//   *   *  
+//  * * * *
+// *   *   *
+public class Pattern17 {
+    public static void main(String[] args) {
+        int n = 3;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (2 * n - 1); j++) {
+                if ((j == (n - i) + 1) || (j == (n + i) - 1))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            for (int j = 2; j <= (2 * n - 1); j++) {
+                if ((j == (n - i) + 1) || (j == (n + i) - 1))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
