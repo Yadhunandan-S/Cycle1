474d8fcf4dfe7b9501d411e88b4d52e472aa5110
Yadhunandan S <139615870+Yadhunandan-S@users.noreply.github.com>
2024-02-18 20:11:25 +0530

    Create PrimeChecker.java

---
+++ PrimeChecker.java
@@ -0,0 +1,30 @@
+public class PrimeChecker {
+    public static void main(String[] args) {
+        int number = 17; // Change this number to test for different inputs
+        boolean isPrime = isPrimeNumber(number);
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+
+    public static boolean isPrimeNumber(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n <= 3) {
+            return true;
+        }
+        if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        }
+        // Starting from 5, we only need to check divisors up to sqrt(n)
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
